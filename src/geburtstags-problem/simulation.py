2a18c67964ca617380faa422f80f1fa25a29bbd4
Julian <julianoberhofer2@gmail.com>
2023-05-07 21:13:57 +0200

    more fancy

--- src/geburtstags-problem/simulation.py
+++ src/geburtstags-problem/simulation.py
@@ -1,5 +1,6 @@
 import random
 from plot import plot
+import time
 
 
 def birthday_paradox_same_day(num_people, num_trials):
@@ -27,11 +28,20 @@ def birthday_paradox_set_day(num_people, num_trials, day):
     return probability
 
 
-if __name__ == '__ain__':
+if __name__ == '__main__':
+    simulation_amount = 150
     data = []
+    # to be fancy:
+    bar_length = 50  # length of the progress bar in characters
+    step_size = 365 / bar_length  # number of steps to take for each character of the bar
     for i in range(1, 365):
-        print(i)
-        data.append([i, birthday_paradox_same_day(i, 10000) *100, birthday_paradox_set_day(i, 10000, 1)* 100])
+        progress = i / 365  # progress as a fraction
+        filled_length = int(round(bar_length * progress))  # length of the filled portion of the bar
+        remaining_length = bar_length - filled_length  # length of the unfilled portion of the bar
+        filled_bar = '█' * filled_length  # character used to represent the filled portion of the bar
+        empty_bar = '░' * remaining_length  # character used to represent the unfilled portion of the bar
+        print(f'\r|{filled_bar}{empty_bar}| {i} / 365 ({progress:.1%})', end='')
+        data.append([i, birthday_paradox_same_day(i, simulation_amount) *100, birthday_paradox_set_day(i, simulation_amount, 1)* 100])
     plot(data)
-
-print(birthday_paradox_set_day(253, 10000, 50))
+# else:
+#    print(birthday_paradox_set_day(253, 10000, 50))
